21b203a472838b676e9d61d5e04a4ef5da128996
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2018-03-15 00:47:29 +0100

    Failed to get Junit to work reverting back to simple helloWorld Test

--- src/main/java/com/uom/game/App.java
+++ src/main/java/com/uom/game/App.java
@@ -1,19 +1,13 @@
 package com.uom.game;
 
-public class App {
-	int a,b;
-	App(int a,int b){
-		this.a = a;
-		this.b = b;
-	}
-	public int add(){
-		return a+b;
-	}
- /*
-	public static void main( String[] args )
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
- */
 }
-
--- src/test/java/com/uom/game/AppTest.java
+++ src/test/java/com/uom/game/AppTest.java
@@ -3,8 +3,6 @@ package com.uom.game;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import junit.framework.Assert;
-import junit.framework.Before;
 
 /**
  * Unit test for simple App.
@@ -12,33 +10,29 @@ import junit.framework.Before;
 public class AppTest 
     extends TestCase
 {
-	App app;
-	@Before
-	public void setUp() throws Exception{
-		app = new App(7,10);
-	}
-	@Test
-	public void testAdd(){
-		Assert.assertEquals(17,app.add());
-	}	
     /**
      * Create the test case
      *
      * @param testName name of the test case
-     
+     */
     public AppTest( String testName )
     {
         super( testName );
     }
 
+    /**
+     * @return the suite of tests being tested
+     */
     public static Test suite()
     {
         return new TestSuite( AppTest.class );
     }
 
-    public void testApp(){
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
         assertTrue( true );
     }
-	
-	*/
 }
