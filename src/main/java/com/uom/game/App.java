5f631dc705815050f4586346871c4029d4c97a4d
Vitaly <vitaly.volozhinov.16@um.edu.mt>
2018-03-14 23:39:09 +0100

    Maths Unit Testing

--- src/main/java/com/uom/game/App.java
+++ src/main/java/com/uom/game/App.java
@@ -10,4 +10,16 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+ 
+    public double sum(double x, double y){
+      return x+y;
+    }
+    public double multiply(double x, double y){
+      return x*y;
+    }
+    public double divide(double x, double y){
+      return x/y;
+    }
+    public double subtract(double x, double y){
+      return x - y;  
 }
--- src/test/java/com/uom/game/AppTest.java
+++ src/test/java/com/uom/game/AppTest.java
@@ -10,29 +10,43 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+    @RunWith(JUnit4.class)
+public class MathTest {
+ 
+   @Rule
+   public Timeout globalTimeout = new Timeout(3000); // 3 seconds max per method tested
+ 
+  com.hmkcode.junit.Math math = new com.hmkcode.junit.Math();
+ 
+  @Test
+  @Ignore
+   public void testAssertNotNull() {
+      org.junit.Assert.assertNotNull("should not be null", math);
+  }
+ 
+    @Test
+    public void testSum(){
+      org.junit.Assert.assertTrue("failure - not equal", math.sum(3, 2) == 5);
+ 
+      //to test timeout
+      /*for (;;) {
+        }*/
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+ 
+    @Test
+    public void testMultiply(){
+       org.junit.Assert.assertTrue("failure - not equal", math.multiply(3, 2) == 6);
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+ 
+    @Test
+    public void testDivide(){
+      double x = 3,y = 2;
+      assertThat("failure - can't divide by 0",y, is(not(0.0)));
+      org.junit.Assert.assertTrue("failure - not equal", math.divide(x, y) == 1.5);
+    }
+ 
+    @Test
+    public void testSubtract(){
+       org.junit.Assert.assertTrue("failure - not equal", math.subtract(3, 2) == 1);
     }
 }
